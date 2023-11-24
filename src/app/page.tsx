30b7cf1d03af69925712418769996896e512ee2c
huseyn-eziz <mastereziz@gmail.com>
2023-11-24 19:48:02 +0400

    nextjs proje started

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { useState } from "react";
+
+export default function Home() {
+  const [inputVal, setInputVal] = useState("")
+  
+  return (
+    <div>
+      <div>
+        <h1>Enter Your Name</h1>
+      </div>
+      <form>
+        <input type="text" placeholder="Type your name..." />
+        <button type="submit">Predict Data</button>
+      </form>
+    </div>
+  );
+}
